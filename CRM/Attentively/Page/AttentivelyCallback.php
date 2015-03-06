077ece380a5a77fb730a75684bfa9a09717ac97f
Edsel <edsel.lopez@jmaconsulting.biz>
2015-03-06 12:25:47 +0530

    AI-25 Reworked for GET instead of POST

--- CRM/Attentively/Page/AttentivelyCallback.php
+++ CRM/Attentively/Page/AttentivelyCallback.php
@@ -4,23 +4,19 @@ require_once 'CRM/Core/Page.php';
 
 class CRM_Attentively_Page_AttentivelyCallback extends CRM_Core_Page {
   function run() {
-    if ($_POST['access_token']) {
-      $values['access_token'] = $_POST['access_token'];
+    $values = array();
+    if ($values['access_token'] = CRM_Utils_Request::retrieve('access_token', 'String')) {
       CRM_Attentively_BAO_Attentively::updateAttentivelyAuth($values);
-      return TRUE;
+    }
+    if (CRM_Attentively_BAO_Attentively::checkAttentivelyAuth() != 'none') {
+      CRM_Core_Session::setStatus(
+        ts('Access Token obtained successfully!'),
+        ts('Complete'), 'success');
     }
     else {
-      if (CRM_Attentively_BAO_Attentively::checkAttentivelyAuth() != 'none') {
-        CRM_Core_Session::setStatus(
-          ts('Access Token obtained successfully!'),
-          ts('Complete'), 'success');
-      }
-      else {
-        CRM_Core_Session::setStatus(
-          ts('There was an error obtaining the Access Token'),
-          ts('Error'), 'error');
-      }
-      
+      CRM_Core_Session::setStatus(
+        ts('There was an error obtaining the Access Token'),
+        ts('Error'), 'error');
     }
     parent::run();
   }
