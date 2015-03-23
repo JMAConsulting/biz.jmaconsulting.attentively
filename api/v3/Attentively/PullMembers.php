5d2c3063b2b298e8f6b2ac6c39c8226bb29e21e2
Edsel <edsel.lopez@jmaconsulting.biz>
2015-03-23 17:45:58 +0530

    AI-31 Refactored API to support v4.4

--- api/v3/Attentively/PullMembers.php
+++ api/v3/Attentively/PullMembers.php
@@ -3,13 +3,18 @@
 /**
  * Attentively.PullMembers API
  *
- * @param array $params
  * @return array API result descriptor
  * @see civicrm_api3_create_success
  * @see civicrm_api3_create_error
  * @throws API_Exception
  */
-function civicrm_api3_attentively_pullmembers($params) {
-  CRM_Attentively_BAO_Attentively::pullMembers();
+function civicrm_api3_attentively_pullmembers() {
+  $count = CRM_Attentively_BAO_Attentively::pullMembers();
+  if ($count) {
+    return civicrm_api3_create_success(ts('Total members fetched from Attentive.ly: ' . $count));
+  }
+  else {
+    return civicrm_api3_create_error(ts('Error while fetching members from Attentive.ly'));
+  }
 }
 
--- api/v3/Attentively/PushMembers.php
+++ api/v3/Attentively/PushMembers.php
@@ -9,7 +9,13 @@
  * @see civicrm_api3_create_error
  * @throws API_Exception
  */
-function civicrm_api3_attentively_pushmembers($params) {
-  CRM_Attentively_BAO_Attentively::pushMembers();
+function civicrm_api3_attentively_pushmembers() {
+  $count = CRM_Attentively_BAO_Attentively::pushMembers();
+  if ($count) {
+    return civicrm_api3_create_success(ts('Total contacts pushed to Attentive.ly: ' . $count));
+  }
+  else {
+    return civicrm_api3_create_success(ts('No contacts were sent to Attentive.ly!'));
+  }
 }
 
