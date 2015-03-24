acc0b167ff692588fe6ef09db5cdc0845454ea22
Edsel <edsel.lopez@jmaconsulting.biz>
2015-03-24 17:51:56 +0530

    AI-31 Added results for cron jobs and reduced lines of code

--- api/v3/Attentively/PushWatchedTerms.php
+++ api/v3/Attentively/PushWatchedTerms.php
@@ -10,6 +10,12 @@
  * @throws API_Exception
  */
 function civicrm_api3_attentively_pushwatchedterms() {
-  CRM_Attentively_BAO_Attentively::pushWatchedTerms();
+  $count = CRM_Attentively_BAO_Attentively::pushWatchedTerms();
+  if ($count) {
+    return civicrm_api3_create_success(ts('Total terms pushed to Attentive.ly: ' . $count));
+  }
+  else {
+    return civicrm_api3_create_error(ts('No terms were sent to Attentive.ly!'));
+  }
 }
 
