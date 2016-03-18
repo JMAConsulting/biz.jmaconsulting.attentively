fecf21d66895f7eed981e5d73d55e9d194d1c51c
Edsel <edsel.lopez@jmaconsulting.biz>
2016-03-18 17:34:04 +0530

    MISC-93 Upgraded civix

--- attentively.civix.php
+++ attentively.civix.php
@@ -4,6 +4,8 @@
 
 /**
  * (Delegated) Implementation of hook_civicrm_config
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
  */
 function _attentively_civix_civicrm_config(&$config = NULL) {
   static $configured = FALSE;
@@ -29,6 +31,7 @@ function _attentively_civix_civicrm_config(&$config = NULL) {
  * (Delegated) Implementation of hook_civicrm_xmlMenu
  *
  * @param $files array(string)
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
  */
 function _attentively_civix_civicrm_xmlMenu(&$files) {
   foreach (_attentively_civix_glob(__DIR__ . '/xml/Menu/*.xml') as $file) {
@@ -38,44 +41,53 @@ function _attentively_civix_civicrm_xmlMenu(&$files) {
 
 /**
  * Implementation of hook_civicrm_install
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
 function _attentively_civix_civicrm_install() {
   _attentively_civix_civicrm_config();
   if ($upgrader = _attentively_civix_upgrader()) {
-    return $upgrader->onInstall();
+    $upgrader->onInstall();
   }
 }
 
 /**
  * Implementation of hook_civicrm_uninstall
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
  */
 function _attentively_civix_civicrm_uninstall() {
   _attentively_civix_civicrm_config();
   if ($upgrader = _attentively_civix_upgrader()) {
-    return $upgrader->onUninstall();
+    $upgrader->onUninstall();
   }
 }
 
 /**
  * (Delegated) Implementation of hook_civicrm_enable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function _attentively_civix_civicrm_enable() {
   _attentively_civix_civicrm_config();
   if ($upgrader = _attentively_civix_upgrader()) {
     if (is_callable(array($upgrader, 'onEnable'))) {
-      return $upgrader->onEnable();
+      $upgrader->onEnable();
     }
   }
 }
 
 /**
  * (Delegated) Implementation of hook_civicrm_disable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ * @return mixed
  */
 function _attentively_civix_civicrm_disable() {
   _attentively_civix_civicrm_config();
   if ($upgrader = _attentively_civix_upgrader()) {
     if (is_callable(array($upgrader, 'onDisable'))) {
-      return $upgrader->onDisable();
+      $upgrader->onDisable();
     }
   }
 }
@@ -88,6 +100,8 @@ function _attentively_civix_civicrm_disable() {
  *
  * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
  *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
  */
 function _attentively_civix_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   if ($upgrader = _attentively_civix_upgrader()) {
@@ -95,6 +109,9 @@ function _attentively_civix_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) 
   }
 }
 
+/**
+ * @return CRM_Attentively_Upgrader
+ */
 function _attentively_civix_upgrader() {
   if (!file_exists(__DIR__.'/CRM/Attentively/Upgrader.php')) {
     return NULL;
@@ -144,6 +161,8 @@ function _attentively_civix_find_files($dir, $pattern) {
  * (Delegated) Implementation of hook_civicrm_managed
  *
  * Find any *.mgd.php files, merge their content, and return.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
  */
 function _attentively_civix_civicrm_managed(&$entities) {
   $mgdFiles = _attentively_civix_find_files(__DIR__, '*.mgd.php');
@@ -159,6 +178,35 @@ function _attentively_civix_civicrm_managed(&$entities) {
 }
 
 /**
+ * (Delegated) Implementation of hook_civicrm_caseTypes
+ *
+ * Find any and return any files matching "xml/case/*.xml"
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function _attentively_civix_civicrm_caseTypes(&$caseTypes) {
+  if (!is_dir(__DIR__ . '/xml/case')) {
+    return;
+  }
+
+  foreach (_attentively_civix_glob(__DIR__ . '/xml/case/*.xml') as $file) {
+    $name = preg_replace('/\.xml$/', '', basename($file));
+    if ($name != CRM_Case_XMLProcessor::mungeCaseType($name)) {
+      $errorMessage = sprintf("Case-type file name is malformed (%s vs %s)", $name, CRM_Case_XMLProcessor::mungeCaseType($name));
+      CRM_Core_Error::fatal($errorMessage);
+      // throw new CRM_Core_Exception($errorMessage);
+    }
+    $caseTypes[$name] = array(
+      'module' => 'biz.jmaconsulting.attentively',
+      'name' => $name,
+      'file' => $file,
+    );
+  }
+}
+
+/**
  * Glob wrapper which is guaranteed to return an array.
  *
  * The documentation for glob() says, "On some systems it is impossible to
@@ -166,7 +214,7 @@ function _attentively_civix_civicrm_managed(&$entities) {
  * result for an empty match is sometimes array() and sometimes FALSE.
  * This wrapper provides consistency.
  *
- * @see http://php.net/glob
+ * @link http://php.net/glob
  * @param string $pattern
  * @return array, possibly empty
  */
@@ -211,5 +259,21 @@ function _attentively_civix_insert_navigation_menu(&$menu, $path, $item, $parent
       }
     }
     return $found;
+  }
+}
+
+/**
+ * (Delegated) Implementation of hook_civicrm_alterSettingsFolders
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function _attentively_civix_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  static $configured = FALSE;
+  if ($configured) return;
+  $configured = TRUE;
+
+  $settingsDir = __DIR__ . DIRECTORY_SEPARATOR . 'settings';
+  if(is_dir($settingsDir) && !in_array($settingsDir, $metaDataFolders)) {
+    $metaDataFolders[] = $settingsDir;
   }
 }
